1aba5a9fb3997bda75ce3490f895efec38c8f7ff
Rishabh Maurya <75687649+RishabhGithub7348@users.noreply.github.com>
2022-01-08 18:33:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,6 @@ import Card from "./Cards";
 import "./index.css";
 import App from "./App";
 
-
-const ncard = (val) => {
-    return(
-       <Card
-       key={val.id} 
-       imgsrc= {val.imgscr}
-       title={val.title}
-       sname = {val.sname}
-       link={val.link}
-        />
-    );
-} 
-
 ReactDOM.render(
 <App />,
   document.getElementById('root') );
